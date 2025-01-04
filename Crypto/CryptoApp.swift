10529f16508ec2ea598ea8d9641f4eaa9e65defe
Marcos Sousa <marcosp.sousa@gmail.com>
2025-01-04 11:06:47 -0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Field on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
