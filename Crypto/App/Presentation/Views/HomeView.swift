76232bcca3c5f87e0aec52d1bb798c76e87bc4f2
Marcos Sousa <marcosp.sousa@gmail.com>
2025-01-04 12:59:24 -0300

    Initial Commit

---
+++ Crypto/App/Presentation/Views/HomeView.swift
@@ -0,0 +1,55 @@
+//
+//  HomeView.swift
+//  Crypto
+//
+//  Created by Field on 04/01/25.
+//
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showPortfolio = false
+
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
